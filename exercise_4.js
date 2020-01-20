19527da280f2fb71bdf25775f0017007a037136f
MARK CLANCY CONSIDINE - STUDENT <G00371583@gmit.ie>
2020-01-20 17:18:37 +0000

    part 1-4;

---
+++ exercise_4.js
@@ -0,0 +1,10 @@
+var yes = true;
+console.log("The value for yes is " + yes);
+var pi = 3.14;
+console.log("the value of pi is " + pi);
+var sentence = "hello world";
+console.log("the sentence says " + sentence);
+var oneToFive = [1, 2, 3, 4, 5];
+console.log("Number one to five = " + oneToFive);
+var y;
+y = [31, "yes", -3.6];
---
+++ exercise_4.ts
@@ -0,0 +1,15 @@
+let yes: boolean = true;
+console.log("The value for yes is "+ yes);
+
+let pi: number=3.14;
+console.log("the value of pi is " + pi);
+
+let sentence: string = "hello world";
+console.log("the sentence says " + sentence);
+
+let oneToFive: number[] = [1,2,3,4,5]; 
+console.log("Number one to five = " + oneToFive);
+
+let y: [number, string, number];
+y = [31, "yes", -3.6];
+console.log("Y Equals " + y);
